5b2cbc277db7ed3e953a282b1bc6451f8241ce39
LiliaASt <lastsatryan2704@mail.ru>
2026-01-12 21:20:08 +0300

    Update Program.cs

--- CarRentalService.Generator.Grpc.Host/Program.cs
+++ CarRentalService.Generator.Grpc.Host/Program.cs
@@ -12,24 +12,9 @@ builder.Services.AddGrpc(options =>
     options.MaxSendMessageSize = 32 * 1024 * 1024;
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Car Rental Generator API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
 
 app.MapGrpcService<CarRentalGrpcGeneratorService>();
 app.MapDefaultEndpoints();
-
-app.MapGet("Car Rental Generator gRPC", () => "Car Rental Generator gRPC Service is running!");
 
 app.Run();
